5be3bd64932b3ca9a6b08d5d3a7fcb74447502ca
Kale Franz <kalefranz@gmail.com>
2022-02-24 12:46:13 -0600

    h1 and background

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
       monsters: [],
       searchField: '',
     };
+
   }
 
   getUsers = async () => {
@@ -24,6 +25,10 @@ class App extends Component {
   componentDidMount() {
     this.getUsers();
   }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
   
   render() {
     const { monsters, searchField } = this.state;
@@ -32,9 +37,10 @@ class App extends Component {
     )
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder='search monsters'
-          handleChange={e => this.setState({searchField: e.target.value})}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
