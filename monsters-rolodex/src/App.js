b558a5b81688b74614ade21cef04d73cc9f53eea
Kale Franz <kalefranz@gmail.com>
2022-02-24 11:52:08 -0600

    monsters react app

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+  }
+
+  getUsers = async () => {
+    const usersResponse = await fetch('https://jsonplaceholder.typicode.com/users')
+    const users = await usersResponse.json();
+    this.setState({ monsters: users })
+  }
+
+  componentDidMount() {
+    this.getUsers();
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <input
+          type='search'
+          placeholder='search monsters'
+          onChange={e => this.setState({searchField: e.target.value})}
+        />
+        <CardList monsters={this.state.monsters} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
