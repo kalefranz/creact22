db7f2f01b4ec5cd338f15272468d1d4b3a0b1131
Kale Franz <kalefranz@gmail.com>
2022-02-24 12:26:06 -0600

    filtered search box

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 
 
 class App extends Component {
@@ -25,14 +26,17 @@ class App extends Component {
   }
   
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
     return (
       <div className="App">
-        <input
-          type='search'
+        <SearchBox
           placeholder='search monsters'
-          onChange={e => this.setState({searchField: e.target.value})}
+          handleChange={e => this.setState({searchField: e.target.value})}
         />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
